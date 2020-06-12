49aa174c37dc44e615bb50160054d44cf3e6cba0
rathod jagadish <46041625+Jagadishrathod@users.noreply.github.com>
2020-06-12 14:43:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,4 +14,20 @@ AppRegistry.registerHeadlessTask('SendSocketID', () =>
     require('./src/utils/SendSocketID'),
 );
 
-AppRegistry.registerComponent(appName, () => Store);
+AppRegistry.registerComponent(docmz, () => Store);
+
+const changeStatus = (socket, namespace) => ({ username, status, room }) => {
+    console.log(`user ${username} wants to change his status to ${status}`);
+
+    Vido_chat.getUser(room, socket.id)
+        .then(user => Video_chat.setUser(room, socket.id, { ...user, status }))
+        .then(() => Video_chat.getUsers(room))
+        .then(users => {
+            if (users === null) return
+            // Notify all the users in the same room
+            namespace.in(room).emit('newUser', { users, username });
+        })
+}
+module.exports = {
+    changeStatus,
+}
